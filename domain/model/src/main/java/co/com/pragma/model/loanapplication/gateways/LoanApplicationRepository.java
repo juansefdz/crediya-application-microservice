f8cb599a38e22ba279a646e7c2af49a4ca3bed7d
juansefdz <jsfm81091@gmail.com>
2025-09-01 08:53:49 -0500

    (feat): se agregan anotaciones a LoanApplicationRequestDTO para menorar el manejo de los datos

--- domain/model/src/main/java/co/com/pragma/model/loanapplication/gateways/LoanApplicationRepository.java
+++ domain/model/src/main/java/co/com/pragma/model/loanapplication/gateways/LoanApplicationRepository.java
@@ -2,9 +2,14 @@ package co.com.pragma.model.loanapplication.gateways;
 
 
 import co.com.pragma.model.loanapplication.LoanApplication;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.awt.print.Pageable;
 
 public interface LoanApplicationRepository {
     Mono<LoanApplication> save(LoanApplication solicitud);
+
+    Flux<LoanApplication> findApplications(Pageable pageable);
 }
 
--- infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/api/dto/LoanApplicationRequestDTO.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/api/dto/LoanApplicationRequestDTO.java
@@ -1,9 +1,7 @@
 package co.com.pragma.api.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,21 +13,23 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public class LoanApplicationRequestDTO {
 
+    @Schema(example = "12345678", description = "número de cédula del cliente")
+    @Size(min = 5, max = 20, message = "El documento debe tener entre 5 y 20 caracteres")
     @NotBlank(message = "El ID del usuario no puede estar vacío.")
     private String usuarioId;
-
+    @Schema(example = "juan.perez@example.com")
     @NotBlank(message = "El email no puede estar vacío.")
     @Email(message = "El formato del email no es válido.")
     private String email;
-
+    @Schema(example = "100000")
     @NotNull(message = "El monto no puede ser nulo.")
     @Positive(message = "El monto debe ser un valor positivo.")
     private BigDecimal monto;
-
+    @Schema(example= "24", description = "el plazo es de (24) meses")
     @NotNull(message = "El plazo no puede ser nulo.")
     @Positive(message = "El plazo debe ser de al menos 1 mes.")
     private Integer plazo;
-
+    @Schema(example = "1", description = "Id del tipo de préstamo. Ej: 1 para Crédito de Libre Inversión")
     @NotBlank(message = "El ID del tipo de préstamo no puede estar vacío.")
     private String prestamoId;
 }
